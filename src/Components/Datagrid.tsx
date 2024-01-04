783259c37ee400b944f3573a4a5a2eb4afd6bafa
Maike <maikonalexandre574@gmail.com>
2024-01-04 16:29:11 -0300

    feat: create datagrid

---
+++ src/Components/Datagrid.tsx
@@ -0,0 +1,12 @@
+export function Datagrid() {
+  return (
+    <div className="col-span-10 mt-4 h-[350px] bg-red-100 overflow-auto">
+      <div
+        id="datagrid"
+        className=" bg-white grid grid-cols-10 text-white text-center items-center transition"
+      >
+        1
+      </div>
+    </div>
+  );
+}

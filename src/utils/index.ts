aba2f33b2482b3e44525ccf7366eb8fbd79fde04
Maike <maikonalexandre574@gmail.com>
2024-01-04 18:41:56 -0300

    feat: add function to copy and save in local storage

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,42 @@
+import { emogi } from "../@types";
+
 export function buildEmoji(arr: any[]) {
   return String.fromCodePoint(...arr.map((code) => parseInt(code, 16)));
+}
+
+export async function copyToClipboard(data: any) {
+  try {
+    await navigator.clipboard.writeText(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export function saveInMemory(emogi: emogi) {
+  let item = localStorage.getItem("slotEmogis");
+
+  if (item) {
+    const data = JSON.parse(item);
+    item = data.filter((e: emogi) => {
+      return JSON.stringify(e) != JSON.stringify({ code: emogi.code });
+    });
+
+    if (data.length > 75) {
+      data.shift();
+    }
+
+    localStorage.setItem(
+      "slotEmogis",
+      JSON.stringify([...data, { code: emogi.code }])
+    );
+    return;
+  }
+
+  localStorage.setItem("slotEmogis", JSON.stringify([{ code: emogi.code }]));
+}
+
+export function getInMemory() {
+  let item = localStorage.getItem("slotEmogis");
+
+  return item ? JSON.parse(item) : [];
 }
